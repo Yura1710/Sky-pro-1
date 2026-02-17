ae075f5edca75315b40744054c45f4091a2d2c71
Yura1710 <taraskin.yu@yandex.ru>
2026-02-17 09:48:24 +0400

    Финальные исправления стиля кода

--- 05_lesson/lesson05_task2.py
+++ 05_lesson/lesson05_task2.py
@@ -3,24 +3,20 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from chromedriver_py import binary_path
 from selenium.webdriver.chrome.service import Service
-
 service = Service(executable_path=binary_path)
 driver = webdriver.Chrome(service=service)
-
 try:
     driver.get("http://uitestingplayground.com/dynamicid")
     print("Страница Dynamic ID загружена")
-    
     time.sleep(2)
-    
-    button = driver.find_element(By.XPATH, "//button[text()='Button with Dynamic ID']")
+    button = driver.find_element(
+        By.XPATH,
+        "//button[text()='Button with Dynamic ID']"
+    )
     print("Кнопка найдена по тексту")
-    
     button.click()
     print("Клик выполнен!")
-    
     time.sleep(3)
-    
 finally:
     driver.quit()
     print("Браузер закрыт")
